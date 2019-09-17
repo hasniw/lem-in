c516341d6edb558dfa55b4136f946e015337c617
Wassim <wassim.hasni42@gmail.com>
2019-09-17 11:05:48 +0200

    Last version

--- srcs/ft_parse_room.c
+++ srcs/ft_parse_room.c
@@ -6,7 +6,7 @@
 /*   By: wahasni <wahasni@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/25 18:40:31 by wahasni           #+#    #+#             */
-/*   Updated: 2019/07/14 01:55:38 by wahasni          ###   ########.fr       */
+/*   Updated: 2019/09/16 11:04:39 by wahasni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -90,74 +90,4 @@ int			ft_room(t_var *var)
 		ft_printf("{red}<---ROOM LINE SUIVANTE--->{reset}\n");
 	}
 	return (0);
-}
-
-
-//
-void    fill_string(char **s, int x, int y, int size)
-{
-        (*s)[y * size + x] = '1';
-        (*s)[x * size + y] = '1';
-}
-
-void    get_coordinates(t_edges *edge, char **split, int *x, int *y)
-{
-        int size;
-
-        size = split_count(split);
-        while (*x < size)
-        {
-                if (!ft_strcmp(split[*x], edge->from)
-                                || !ft_strcmp(split[*x], edge->to))
-                {
-                        (*y)++;
-                        break ;
-                }
-                (*y)++;
-                (*x)++;
-        }
-        while (*y < size)
-        {
-                if (!ft_strcmp(split[*y], edge->from)
-                                || !ft_strcmp(split[*y], edge->to))
-                        break ;
-                (*y)++;
-        }
-}
-
-int             assign(t_edges *edge, t_vertices *vertice, char **s, int size)
-{
-        t_edges *tmp;
-        int             x;
-        int             y;
-        char    **split;
-
-        split = NULL;
-        if (!(split = split_vertices(vertice)))
-                return (0);
-        tmp = edge;
-        while (tmp)
-        {
-                x = 0;
-                y = 0;
-                get_coordinates(tmp, split, &x, &y);
-                fill_string(s, x, y, size);
-                tmp = tmp->next;
-        }
-        return (free_split(split, 1));
-}
-
-char    *stringify(t_data *data)
-{
-        char    *s;
-        size_t  size;
-
-        size = 0;
-        size = vertices_len(data->vertices);
-        if (!(s = ft_strnew(sizeof(char) * size * size)))
-                return (NULL);
-        ft_memset(s, '0', size * size);
-        if (!assign(data->edges, data->vertices, &s, size))
-                return (NULL);
-        return (s);
 }
